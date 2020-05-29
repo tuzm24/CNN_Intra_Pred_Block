580647523f59520ec9f4a97e6d9ac41fc0aa3fb0
YangwooKim <tuzm24@naver.com>
2020-05-30 07:13:18 +0900

    Mode Norm bug fix

--- model/rdn_intramode.py
+++ model/rdn_intramode.py
@@ -75,8 +75,7 @@ class RDN(nn.Module):
             nn.ReLU()
         ])
         self.UPNet = nn.Sequential(*[
-            nn.Conv2d(G0, 95, kSize, padding=(kSize - 1) // 2, stride=1),
-            nn.Softmax(dim=1)
+            nn.Conv2d(G0, 95, kSize, padding=(kSize - 1) // 2, stride=1)
         ])
 
 
